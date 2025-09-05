25a8d31f8b625e9a67431bbef377832665ad0e35
Dev Hari Ojha <ojha6773@gmail.com>
2025-09-05 17:08:01 +0530

    ok

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Webcam from 'react-webcam';
+
+import PhotoBooth from './PhotoBooth';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <PhotoBooth />
+      </div>
+  );
+};
+
+export default Hero;
